ed325a3ff52b86702b65fc3acefe53acf2348937
Leo Riegler <leo.riegler1@gmail.com>
2020-04-07 18:19:35 -0300

    Product

---
+++ src/main/java/com/example/webservicespring/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.example.webservicespring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.webservicespring.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+	
+}
